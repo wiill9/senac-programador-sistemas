a8f33a1a3b3307ffbd052a6fbefb549784c3a5f7
WILDER RIBEIRO DOS SANTOS <wilder.rsantos@senacsp.edu.br>
2025-04-11 22:22:36 -0300

    confiança

---
+++ ListaAtividade/Form1.cs
@@ -0,0 +1,10 @@
+namespace ListaAtividade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ListaAtividade/bando de dados/banco de dados2.sql
@@ -0,0 +1,13 @@
+CREATE TABLE atividade (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(100) NOT NULL,
+    situacao INT NOT NULL DEFAULT 0
+);
+
+insert into aitividade (titulo) values (@titulo);
+
+update atividade set situacao = @situacao where id = @id; 
+
+select * from atividade where situacao = 1; 
+
+select * from atividade where situacao = 0;
